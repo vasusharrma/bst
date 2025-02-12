2d461260e09747b2c072613bc0846b76398bf859
vasusharrma <sharmavashu099@gmail.com>
2025-02-12 23:23:51 +0530

    post pre in order traversal

--- 01.cpp
+++ 01.cpp
@@ -15,32 +15,68 @@ public:
 };
 
 Node *createTree() {
-  int data;
-  cout << "Enter the value : " << endl;
-  cin >> data;
-  if (data == -1) {
-    return nullptr;
-  }
 
-  // Step 1
+  int data;
+  cout << "vaule enter : " << endl;
+  cin >> data;
+
+  if (data == -1)
+    return nullptr;
 
   Node *root = new Node(data);
 
-  // Create left subtree
+  cout << root->data << " left : " << " ";
   root->left = createTree();
-
-  // Create right subtree
+  cout << root->data << " right : " << " ";
   root->right = createTree();
 
   return root;
+};
 
-  //
+void inOrderTraversal(Node *root) {
+
+  if (root == nullptr) {
+    return;
+  }
+
+  inOrderTraversal(root->left);
+
+  cout << root->data << " ";
+
+  inOrderTraversal(root->right);
+}
+
+void preOrderTraversal(Node *root) {
+
+  if (root == nullptr) {
+    return;
+  }
+
+  cout << root->data << " ";
+  preOrderTraversal(root->left);
+  preOrderTraversal(root->right);
+}
+
+void postOrderTraversal(Node *root) {
+  if (root == nullptr) {
+    return;
+  }
+
+  postOrderTraversal(root->left);
+  postOrderTraversal(root->right);
+  cout << root->data << " ";
 }
 
 int main() {
 
   Node *root = createTree();
-  cout << "root data is : " << root->data;
+
+  cout << endl << "preOrderTraversal" << endl;
+  preOrderTraversal(root);
+  cout << endl << "inOrderTraversal" << endl;
+  inOrderTraversal(root);
+  cout << endl << "postOrderTraversal" << endl;
+  postOrderTraversal(root);
 
   return 0;
 }
